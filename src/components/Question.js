cde9fd6f5544be3f0351da62095d005f1235339e
Belinda <belindaseshie5@gmail.com>
2024-08-01 12:28:26 +0000

    made changes to the question and results components

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,307 +1,4 @@
-// import React, { useState, useEffect } from 'react';
-// // import { useNavigate, useParams } from 'react-router-dom';
-// import { useParams } from 'react-router-dom';
-// import axios from 'axios';
-
-// // Function to fetch questions based on the API URL
-// const fetchQuestionsForQuiz = async (apiUrl, retries = 3, delay = 1000) => {
-//   try {
-//     const response = await axios.get(apiUrl);
-//     console.log('Fetched questions:', response.data.results);
-//     return response.data.results.map(question => ({
-//       question: question.question,
-//       options: [...question.incorrect_answers, question.correct_answer].sort(() => Math.random() - 0.5), // Shuffle options
-//       answer: question.correct_answer,
-//     }));
-//   } catch (error) {
-//     if (error.response && error.response.status === 429 && retries > 0) {
-//       console.warn('Rate limit exceeded. Retrying...');
-//       await new Promise(res => setTimeout(res, delay)); // Wait before retrying
-//       return fetchQuestionsForQuiz(apiUrl, retries - 1, delay * 2); // Gradual Retry Intervals
-//     } else {
-//       console.error('Error fetching trivia questions:', error);
-//       throw error;
-//     }
-//   }
-// };
-
-// // Function to get quiz data based on quizId
-// const getQuizData = async (quizId) => {
-//   try {
-//     const response = await fetch('/quiz.json');
-//     if (!response.ok) throw new Error('Network response was not ok');
-//     const quizzes = await response.json();
-//     console.log('Fetched quizzes:', quizzes);
-
-//     const quiz = quizzes.find(q => q.id === quizId);
-//     console.log('Selected quiz:', quiz);
-
-//     if (quiz) {
-//       return {
-//         title: quiz.title,
-//         questions: await fetchQuestionsForQuiz(quiz.api_url),
-//       };
-//     }
-//     return { title: '', questions: [] };
-//   } catch (error) {
-//     console.error('Error fetching quiz data:', error);
-//     return { title: '', questions: [] };
-//   }
-// };
-
-// // Function to get username from localStorage
-// const getUserNameFromLocalStorage = () => {
-//   return localStorage.getItem('userName') || 'User'; // Default to 'User' if not found
-// };
-
-// const Question = () => {
-//   const [questions, setQuestions] = useState([]);
-//   const [quizTitle, setQuizTitle] = useState(''); // Added state for quiz title
-//   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-//   const [selectedOption, setSelectedOption] = useState('');
-//   const [score, setScore] = useState(0);
-//   const [showScore, setShowScore] = useState(false);
-//   const [feedback, setFeedback] = useState(null);
-//   const [totalTimeLeft, setTotalTimeLeft] = useState(300); // Total time for the quiz in seconds
-//   // const navigate = useNavigate();
-//   const { quizId } = useParams();
-
-//   useEffect(() => {
-//     console.log('Quiz ID:', quizId);
-//     const fetchQuestions = async () => {
-//       try {
-//         const { title, questions: quizQuestions } = await getQuizData(quizId);
-//         console.log('Questions fetched:', quizQuestions);
-//         setQuizTitle(title); // Set the quiz title
-//         setQuestions(quizQuestions);
-//       } catch (error) {
-//         console.error('Error fetching questions:', error);
-//       }
-//     };
-  
-//     fetchQuestions();
-//   }, [quizId]);
-  
-//   useEffect(() => {
-//     console.log('Questions length:', questions.length); 
-//   }, [questions]);
-
-//   useEffect(() => {
-//     if (totalTimeLeft === 0) {
-//       setShowScore(true);
-//     }
-//     const timer = totalTimeLeft > 0 && setInterval(() => setTotalTimeLeft(prevTime => prevTime - 1), 1000);
-//     return () => clearInterval(timer);
-//   }, [totalTimeLeft]);
-
-//   // const handleLogout = async () => {
-//   //   // Display a confirmation dialog
-//   //   const confirmed = window.confirm('Logging Out');
-    
-//   //   if (confirmed) {
-//   //     try {
-//   //       await fetch('http://localhost:8000/api/logouts/', {
-//   //         method: 'POST',
-//   //         headers: {
-//   //           'Content-Type': 'application/json',
-//   //         },
-//   //       });
-//   //       localStorage.removeItem('authToken');
-//   //       localStorage.removeItem('userName');
-//   //       navigate('/login');
-//   //     } catch (error) {
-//   //       console.error('Logout failed:', error);
-//   //     }
-//   //   }
-//   // };
-  
-
-//   // Function to fetch user ID from localStorage 
-//   const getUserIdFromLocalStorage = () => {
-//     return localStorage.getItem('userId') || '1'; // Default to '1' if not found
-//   };
-
-//   // Function to fetch quiz data
-//   const fetchQuizData = async (quizId) => {
-//     try {
-//       const response = await fetch('/quiz.json');
-//       if (!response.ok) throw new Error('Network response was not ok');
-//       const quizzes = await response.json();
-
-//       const quiz = quizzes.find(q => q.id === quizId);
-//       return quiz ? { id: quiz.id, title: quiz.title } : null; 
-//     } catch (error) {
-//       console.error('Error fetching quiz data:', error);
-//       return null;
-//     }
-//   };
-
-//   // Function to submit score
-//   const submitScore = async (score) => {
-//     try {
-//       const userName = getUserNameFromLocalStorage();
-  
-//       if (!userName) {
-//         throw new Error('User name not found');
-//       }
-  
-//       const userId = await getUserIdFromLocalStorage();
-//       const quizData = await fetchQuizData(quizId);
-  
-//       if (!quizData) {
-//         throw new Error('Quiz data not found');
-//       }
-  
-//       await axios.post('http://localhost:8000/api/scores/', {
-//         account: userId,        
-//         quiz: { title: quizData.title },  
-//         score: score,           
-//       });
-  
-//       console.log('Score submitted successfully');
-//     } catch (error) {
-//       console.error('Error submitting score:', error);
-//       console.log(error.response?.data); 
-//     }
-//   };
-  
-//   const handleOptionChange = (e) => {
-//     setSelectedOption(e.target.value);
-//   };
-
-//   const handleNextQuestion = () => {
-//     if (selectedOption === questions[currentQuestionIndex]?.answer) {
-//       setScore(score + 1 );
-//       setFeedback('Correct!');
-//     } else {
-//       setFeedback(`Incorrect: The correct answer is ${questions[currentQuestionIndex]?.answer}`);
-//     }
-
-//     setTimeout(() => {
-//       setSelectedOption('');
-//       setFeedback(null);
-//       const nextQuestionIndex = currentQuestionIndex + 1;
-//       if (nextQuestionIndex < questions.length) {
-//         setCurrentQuestionIndex(nextQuestionIndex);
-//       } else {
-//         submitScore(score ); 
-//         setShowScore(true);
-//       }
-//     }, 2000);
-//   };
-
-//   const Navbar = () => (
-//     <nav className="navbar navbar-expand-lg bg-body-tertiary">
-//       <div className="container-fluid">
-//         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-//           <span className="navbar-toggler-icon"></span>
-//         </button>
-//         <a className="navbar-brand mx-3" href="/">VIRTUAL EXAMINER</a>
-//         <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
-//           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-//             <li className="nav-item">
-//               <a className="nav-link active" aria-current="page" href="/browse">QUIZZES</a>
-//             </li>
-//             <li className="nav-item">
-//               <a className="nav-link" href="/result">HISTORY</a>
-//             </li>
-//           </ul>
-//           <form className="d-flex" role="search">
-//             <input className="form-control me-2" type="search" placeholder="Search course or topic" aria-label="Search" />
-//             <button className="btn btn-outline-light me-3" type="submit">Search</button>
-//           </form>
-//           {/* <div className="d-flex align-items-center me-3">
-//             <button className="btn btn-outline-light me-3" type="button" onClick={handleLogout}>LOGOUT</button>
-//           </div> */}
-//         </div>
-//       </div>
-//     </nav>
-//   );
-
-//   if (showScore) {
-//     return (
-//       <div>
-//         <Navbar />
-//         <div className="container mt-5">
-//           <div className="row justify-content-center">
-//             <div className="col-lg-8">
-//               <div className="card quiz text-center">
-//                 <div className="card-body score">
-//                   <h1 className="card-title">Your Score</h1>
-//                   <h5 className={`card-text ${score > (questions.length / 2) ? 'text-success' : 'text-danger'}`}>
-//                     You scored {score} out of {questions.length}
-//                   </h5>
-//                   <a href="/browse" style={{ backgroundColor: '#0F4C75', color: '#fff' }} className="btn mt-3">Back to Quizzes</a>
-//                 </div>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     );
-//   }
-
-//   return (
-//     <div>
-//       <Navbar />
-//       <div className="container mt-5">
-//         {questions.length > 0 ? (
-//           <div className="row justify-content-center">
-//             <div className="col-lg-12">
-//               <div className="card quiz">
-//                 <div className="card-body">
-//                   <div className="d-flex mt-5">
-//                     <h3 style={{ marginRight: '5%', color: '#0F4C75', marginLeft: '5%' }}>{quizTitle}</h3>
-//                     <h4 style={{ marginLeft: 'auto', marginRight: '5%' }}>Question {currentQuestionIndex + 1} of {questions.length}</h4>
-//                     <h4 style={{ marginLeft: 'auto', marginRight: '5%' }}>Time left: {Math.floor(totalTimeLeft / 60)}:{totalTimeLeft % 60 < 10 ? `0${totalTimeLeft % 60}` : totalTimeLeft % 60} minutes</h4>
-                    
-//                   </div>
-//                   <h4 style={{ marginLeft: '5%' }} className="card-title mt-5 mb-4">
-//                     {questions[currentQuestionIndex]?.question}
-//                   </h4>
-//                   <form style={{ marginLeft: '5%' }}>
-//                     {questions[currentQuestionIndex]?.options.map((option, index) => (
-//                       <div key={index} className="form-check">
-//                         <input
-//                           className="form-check-input"
-//                           type="radio"
-//                           name="option"
-//                           value={option}
-//                           checked={selectedOption === option}
-//                           onChange={handleOptionChange}
-//                         />
-//                         <label className="form-check-label">
-//                           {option}
-//                         </label>
-//                       </div>
-//                     ))}
-//                   </form>
-//                   {feedback && (
-//                     <div className={`mt-3 text-${feedback === 'Correct!' ? 'success' : 'danger'}`}>
-//                       <h3 style={{ marginLeft: '5%' }}>{feedback}</h3>
-//                     </div>
-//                   )}
-//                   <button className="btn quizbutton px-5" style={{ backgroundColor: '#0F4C75', color: '#fff', marginLeft: '80%' }} onClick={handleNextQuestion}>Next</button>
-//                 </div>
-//               </div>
-//             </div>
-//           </div>
-//         ) : (
-//           <p>Loading questions...</p>
-//         )}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Question;
-
-
-
-
-
 import React, { useState, useEffect } from 'react';
-// import { useNavigate, useParams } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 
@@ -393,10 +90,7 @@ const Question = () => {
     return () => clearInterval(timer);
   }, [totalTimeLeft]);
 
-  // Function to fetch user ID from localStorage 
-  const getUserIdFromLocalStorage = () => {
-    return localStorage.getItem('userId') || '1';
-  };
+  
 
   const fetchQuizData = async (quizId) => {
     try {
@@ -412,34 +106,40 @@ const Question = () => {
     }
   };
 
+  
   const submitScore = async (score) => {
     try {
-      const userName = getUserNameFromLocalStorage();
-  
-      if (!userName) {
-        throw new Error('User name not found');
-      }
-  
-      const userId = await getUserIdFromLocalStorage();
-      const quizData = await fetchQuizData(quizId);
-  
-      if (!quizData) {
-        throw new Error('Quiz data not found');
-      }
-  
-      await axios.post('http://localhost:8000/api/scores/', {
-        account: userId,        
-        quiz: { title: quizData.title },  
-        score: score,           
-      });
-  
-      console.log('Score submitted successfully');
+        const userName = getUserNameFromLocalStorage();
+        if (!userName) {
+            throw new Error('User name not found');
+        }
+
+        const quizData = await fetchQuizData(quizId);
+
+        if (!quizData) {
+            throw new Error('Quiz data not found');
+        }
+
+        console.log("Submitting score:");
+        console.log(`userName: ${userName}`);
+        console.log(`quiz: ${JSON.stringify(quizData)}`);
+        console.log(`score: ${score}`);
+
+        await axios.post('http://localhost:8000/api/scores/', {
+            userName: userName,
+            quiz: { title: quizData.title || "Unknown Quiz" },
+            score: score,
+        });
+
+        console.log('Score submitted successfully');
     } catch (error) {
-      console.error('Error submitting score:', error);
-      console.log(error.response?.data); 
+        console.error('Error submitting score:', error);
+        console.log(error.response?.data);
     }
-  };
+};
+
   
+
   const handleOptionChange = (e) => {
     setSelectedOption(e.target.value);
   };
@@ -466,43 +166,46 @@ const Question = () => {
   };
 
   const downloadQuestions = () => {
-    // Create an array to hold the text lines
     const textLines = [];
-  
-    // Add headers
-    textLines.push('Question\tOption 1\tOption 2\tOption 3\tOption 4\tCorrect Answer');
-  
-    // Add rows for each question
+
+    // // Add the header row
+    // textLines.push('Question\tOption 1\tOption 2\tOption 3\tOption 4\tCorrect Answer');
+
     questions.forEach(question => {
-      const options = [...question.options];
-      // Ensure there are exactly 4 options (adjust if needed)
-      while (options.length < 4) options.push('');
-  
-      // Join question and options with tab characters
-      textLines.push([
-        question.question,
-        options[0],
-        options[1],
-        options[2],
-        options[3],
-        question.answer
-      ].join('\t'));
+        const options = [...question.options];
+        while (options.length < 4) options.push('');
+
+        const formattedOptions = options.map(option =>
+            option === question.answer
+                ? `**${option}**`  // Highlight correct answer with asterisks
+                : option
+        );
+
+       
+        textLines.push([
+            question.question,
+            formattedOptions[0],
+            formattedOptions[1],
+            formattedOptions[2],
+            formattedOptions[3],
+            // formattedOptions.find(opt => opt.startsWith('**')) // Correct answer highlighted
+        ].join('\t\t'));
     });
-  
-    // Join the lines with newline characters
-    const textString = textLines.join('\n');
-    
-    // Create a blob from the text string
+
+
+    const textString = textLines.join('\n\n\n\n');
+
     const blob = new Blob([textString], { type: 'text/plain' });
-    
+
     // Create a link element
     const link = document.createElement('a');
     link.href = URL.createObjectURL(blob);
     link.download = `${quizTitle}-questions.txt`;
-    
-    // Programmatically click the link to trigger the download
+
+    // Click the link to trigger the download
     link.click();
-  };
+};
+
   
 
   const Navbar = () => (
@@ -525,9 +228,6 @@ const Question = () => {
             <input className="form-control me-2" type="search" placeholder="Search course or topic" aria-label="Search" />
             <button className="btn btn-outline-light me-3" type="submit">Search</button>
           </form>
-          {/* <div className="d-flex align-items-center me-3">
-            <button className="btn btn-outline-light me-3" type="button" onClick={handleLogout}>LOGOUT</button>
-          </div> */}
         </div>
       </div>
     </nav>
@@ -597,7 +297,7 @@ const Question = () => {
                       <h3 style={{ marginLeft: '5%' }}>{feedback}</h3>
                     </div>
                   )}
-                  <button className="btn quizbutton px-5" style={{ backgroundColor: '#0F4C75', color: '#fff', marginLeft: '80%' }} onClick={handleNextQuestion}>Next</button>
+                  <button className="btn quizbutton px-5" style={{ backgroundColor: '#0F4C75', color: '#fff',marginLeft: '5%' }} onClick={handleNextQuestion}>Next</button>
                 </div>
               </div>
             </div>
--- src/components/Result.js
+++ src/components/Result.js
@@ -1,106 +1,3 @@
-// import React, { useState, useEffect } from 'react';
-// import { Link, useNavigate } from 'react-router-dom';
-// import axios from 'axios';
-
-// const Statistics = () => {
-//   const [quizzes, setQuizzes] = useState([]);
-//   const [error, setError] = useState(null);
-//   const [loading, setLoading] = useState(true);
-//   const navigate = useNavigate();
-
-//   const handleLogout = async () => {
-//     try {
-//       await fetch('http://localhost:8000/api/logouts/', {
-//         method: 'POST',
-//         headers: {
-//           'Content-Type': 'application/json',
-//         },
-//       });
-//       navigate('/');
-//     } catch (error) {
-//       console.error('Logout failed:', error);
-//     }
-//   };
-
-//   useEffect(() => {
-//     const fetchScores = async () => {
-//       try {
-//         const response = await axios.get('http://localhost:8000/api/scores/');
-//         setQuizzes(response.data);
-//       } catch (error) {
-//         console.error('Error fetching quiz scores:', error);
-//         setError('Failed to load quiz scores.');
-//       } finally {
-//         setLoading(false);
-//       }
-//     };
-
-//     fetchScores();
-//   }, []);
-
-//   if (loading) return <div className="text-center mt-5">Loading...</div>;
-
-//   return (
-//     <div>
-//       <nav className="navbar navbar-expand-lg bg-body-tertiary">
-//         <div className="container-fluid">
-//           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-//             <span className="navbar-toggler-icon"></span>
-//           </button>
-//           <Link className="navbar-brand mx-3" to="/">VIRTUAL EXAMINER</Link>
-//           <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
-//             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-//               <li className="nav-item">
-//                 <Link className="nav-link" aria-current="page" to="/browse">QUIZZES</Link>
-//               </li>
-//               <li className="nav-item">
-//                 <Link className="nav-link active" to="/result">HISTORY</Link>
-//               </li>
-//             </ul>
-//             <div className="d-flex align-items-center me-3">
-//               <button className="btn btn-outline-light me-3" type="button" onClick={handleLogout}>LOGOUT</button>
-//             </div>
-//           </div>
-//         </div>
-//       </nav>
-//       <div className="container mt-5">
-//         <h1 className="mb-4" style={{ color: '#0F4C75' }}>HISTORY</h1>
-//         {error && <div className="alert alert-danger" role="alert">{error}</div>}
-//         <table className="table table-striped">
-//           <thead>
-//             <tr>
-//               <th scope="col">Quiz Name</th>
-//               <th scope="col">Date Taken</th>
-//               <th scope="col">Score</th>
-//             </tr>
-//           </thead>
-//           <tbody>
-//               {quizzes.length > 0 ? (
-//                 quizzes.map((item, index) => (
-//                   <tr key={index}>
-//                     <td>{item.quiz.title}</td>
-//                     <td>{new Date(item.date_taken).toLocaleDateString()}</td>
-//                     <td>{item.score !== undefined ? item.score : 'N/A'}</td>
-//                   </tr>
-//                 ))
-//               ) : (
-//                 <tr>
-//                   <td colSpan="3" className="text-center">No quiz scores available</td>
-//                 </tr>
-//               )}
-//           </tbody>
-
-//         </table>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Statistics;
-
-
-
-
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
@@ -116,16 +13,19 @@ const Statistics = () => {
     const fetchScores = async () => {
       try {
         const storedUserName = localStorage.getItem('userName');
+        console.log('Stored username:', storedUserName);
+
         if (!storedUserName) {
-          navigate('/login'); // Redirect to login if no username
+          navigate('/login'); 
           return;
         }
         
         setUserName(storedUserName);
         const response = await axios.get('http://localhost:8000/api/scores/', {
           params: { username: storedUserName, t: new Date().getTime() },
+          
         });
-        
+        console.log(response.params)
         setQuizzes(response.data);
       } catch (error) {
         console.error('Error fetching quiz scores:', error);
@@ -169,20 +69,28 @@ const Statistics = () => {
               <th scope="col">Quiz Name</th>
               <th scope="col">Date Taken</th>
               <th scope="col">Score</th>
+              <th scope="col">Remarks</th>
             </tr>
           </thead>
           <tbody>
             {quizzes.length > 0 ? (
-              quizzes.map((item, index) => (
-                <tr key={index}>
-                  <td>{item.quiz.title}</td>
-                  <td>{new Date(item.date_taken).toLocaleDateString()}</td>
-                  <td>{item.score !== undefined ? item.score : 'N/A'}</td>
-                </tr>
-              ))
+              quizzes.map((item, index) => {
+                const totalQuestions = item.quiz.total_questions; // Ensure this field is returned from your API
+                const passingScore = totalQuestions / 2;
+                const remarks = item.score > passingScore ? 'Excellent' : 'Keep Practicing, You Can Do It';
+                
+                return (
+                  <tr key={index}>
+                    <td>{item.quiz.title}</td>
+                    <td>{new Date(item.date_taken).toLocaleDateString()}</td>
+                    <td>{item.score !== undefined ? item.score : 'N/A'}</td>
+                    <td>{remarks}</td>
+                  </tr>
+                );
+              })
             ) : (
               <tr>
-                <td colSpan="3" className="text-center">No quiz scores available</td>
+                <td colSpan="4" className="text-center">No quiz scores available</td>
               </tr>
             )}
           </tbody>
